c78010a187bc18a15a02660477e0ff2696c7e4bf
Noam Shemesh <noam.s@matomy.com>
2014-07-03 14:30:10 +0300

    change index to initsequelize

---
+++ models/initsequelize.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * Created by noam on 7/2/14.
+ */
+
+var fs        = require('fs')
+  , path      = require('path')
+  , Sequelize = require('sequelize')
+  , lodash    = require('lodash')
+  , sequelize = new Sequelize('sequelize_test', 'root', null)
+  , db        = {}
+
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== 'initsequelize.js')
+  })
+  .forEach(function(file) {
+    var model = sequelize.import(path.join(__dirname, file))
+    db[model.name] = model
+  })
+
+Object.keys(db).forEach(function(modelName) {
+  if ('associate' in db[modelName]) {
+    db[modelName].associate(db)
+  }
+})
+
+module.exports = lodash.extend({
+  sequelize: sequelize,
+  Sequelize: Sequelize
+}, db)
